663dbe5b7e91229e31d2e75dd87a8accd7a1e4ea
glucatv <78474060+glucatv@users.noreply.github.com>
2024-05-29 09:22:16 +0200

    Create 01-first_program.c

---
+++ C/01-first_program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	printf("This is my first C program\n");
+
+	/* this is a coment*/
+
+	/* in  Python
+	x = 1  
+	x = 'a string'
+	*/
+	
+	int n = 1;
+	float x;
+	
+	x = n+1;
+	
+	printf("the value of variable x is %f.\n", x);
+	printf("the value of variable n is %d.\n", n); 
+}
